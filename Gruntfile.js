9082125b62304d91d2cf3cf0aae799261e676e96
Rai Butera <rai@rbutera.com>
2014-06-09 20:43:55 +0100

    following 2.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,8 +71,33 @@ module.exports = function (grunt) {
         hostname: 'localhost',
         livereload: 35729
       },
+      proxies: [
+      {
+        context: '/api',
+        host: 'localhost',
+        port: '3000'
+      }],
       livereload: {
         options: {
+          middleware: function (connect, options) {
+                        if (!Array.isArray(options.base)) {
+                            options.base = [options.base];
+                        }
+
+                        // Setup the proxy
+                        var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
+
+                        // Serve static files.
+                        options.base.forEach(function(base) {
+                            middlewares.push(connect.static(base));
+                        });
+
+                        // Make directory browse-able.
+                        var directory = options.directory || options.base[options.base.length - 1];
+                        middlewares.push(connect.directory(directory));
+
+                        return middlewares;
+                    },
           open: true,
           base: [
             '.tmp',
@@ -354,6 +379,7 @@ module.exports = function (grunt) {
       'wiredep',
       'concurrent:server',
       'autoprefixer',
+      'configureProxies:server',
       'connect:livereload',
       'watch'
     ]);
@@ -367,6 +393,7 @@ module.exports = function (grunt) {
   grunt.registerTask('test', [
     'clean:server',
     'concurrent:test',
+    'connect:livereload',
     'autoprefixer',
     'connect:test',
     'karma'
--- edges.js
+++ edges.js
@@ -8,10 +8,6 @@
  * Controller of the swFrontApp
  */
 angular.module('swFrontApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('EdgesController', function ($scope) {
+    
   });
