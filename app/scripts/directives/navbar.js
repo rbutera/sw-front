f27e88b05dea4a0022347a235bbafeff30e5c612
Rai Butera <rai@rbutera.com>
2014-06-10 12:13:58 +0100

    added navbar directive

---
+++ app/scripts/directives/navbar.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var app = angular.module('swFrontApp');
+
+app.directive('navbar', [function () {
+  return {
+    restrict: 'E',
+    templateUrl: "views/navbar.html",
+    controller: 'NavigationController'
+  };
+}])
